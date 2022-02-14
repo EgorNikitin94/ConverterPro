ffcb11f377f9c18f259f58135ba6cf1043530776
EgorNikitin94 <egornik1994@yandex.ru>
2022-02-14 23:17:38 +0300

    Initial Commit

---
+++ ConverterPro/ConverterProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterProApp.swift
+//  ConverterPro
+//
+//  Created by Егор Никитин on 14.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ConverterProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
